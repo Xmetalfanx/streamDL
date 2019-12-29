dec4ed3dced66873b5121e108554c3be1a4ed015
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-12-29 16:10:14 -0500

    some UI changes

--- downloadStream.sh
+++ downloadStream.sh
@@ -12,18 +12,19 @@ function userPrompt()
   read -p "Press [Enter] to continue "
 }
 
+# Main function
 function main()
 {
     clear
     echo -e "This will (hopefully) Download streaming media into a mp4 file"
-    read -p "Stream URL:" streamURL
+    read -p "Stream URL: " streamURL
 
     read -p "Is this a TV program?(y/n)" tvAnswer
     case $tvAnswer in  
         [Yy])  
-            read -p "Show Name:" showName
-            read -p "Season:" seasonNum
-            read -p "Episode Number" episodeNum
+            read -p "Show Name: " showName
+            read -p "Season: " seasonNum
+            read -p "Episode Number: " episodeNum
 
             outputFileName="${showName}S${seasonNum}Ep${episodeNum}"
 
