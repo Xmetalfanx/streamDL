4508fe713f489acc0f727bc9f2297c1882c79033
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-12-31 14:41:38 -0500

    Added some info and comments on video quality

--- ytDownload.sh
+++ ytDownload.sh
@@ -10,17 +10,19 @@ audio="140"
 HDVideo="298"
 
 
-#137          mp4        1920x1080  1080p 3047k , avc1.640028, 30fps, video only
+#137---mp4  ---1920x1080  1080p 3047k , avc1.640028, 30fps, video only
 # 1080p
 FHDVideo1="137"
 
-#299          mp4        1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
+#299 --- mp4  ---1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
 # 1080p
+# 60 FPS 
 FHDVideo2="299"
 
 # 720pDownload
 function hdDownload()
 {
+    echo -e "Downloading at 720p"
     youtube-dl -f $HDVideo+$audio "$ytLink"
 
 }
@@ -28,13 +30,16 @@ function hdDownload()
 # 1080p Download
 function fhdDownload1()
 {
+    echo -e "Downloading at 1080p - Type 1"
     youtube-dl -f $FHDVideo1+$audio "$ytLink"
 
 }
 
 # Alternative 1080p
+# 60 FPS 
 function fhdDownload2()
 {
+    echo -e "Downloading at 1080p - Type 2"
     youtube-dl -f $FHDVideo2+$audio "$ytLink"
 
 }
@@ -50,7 +55,7 @@ function mainMenu() {
         echo
         echo -e "1. \t 720p Video Download"
         echo -e "2. \t 1080p Video Download 1"
-        echo -e "3. \t 1080p Video Download 2"
+        echo -e "3. \t 1080p Video Download 2 (60FPS Video)"
 
         echo -e "4. \t Audio only"
 
