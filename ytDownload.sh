8c84735cdf97cf80928e90d542c730f3739b3bea
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-06-06 14:24:46 -0400

    Retooled Youtube Download script

--- ytDownload.sh
+++ ytDownload.sh
@@ -1,58 +1,35 @@
 #!/bin/bash
 
-# AUDIO
+# KEEP GLOBAL - AUDIO
 audio="140"
 
-# VIDEO
-
-# 298 mp4        1280x720
-# 720
-HDVideo="298"
-
-
-#137---mp4  ---1920x1080  1080p 3047k , avc1.640028, 30fps, video only
-# 1080p
-FHDVideo1="137"
-
-#299 --- mp4  ---1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
-# 1080p
-# 60 FPS 
-FHDVideo2="299"
-
-# 720pDownload
-function hdDownload()
+# New idea for meta function
+function ytDownloadMeta()
 {
-    echo -e "Downloading at 720p"
-    youtube-dl -f $HDVideo+$audio "$ytLink"
+    case video in
+
+        298) echo -e "Downloading at 720p" ;;
+        137) echo -e "Downloading at 1080p - Type 1" ;;
+        299) echo -e "Downloading at 1080p - Type 2" ;;
+
+    esac
+
+    youtube-dl -f $video+$audio "$ytLink"
 
 }
-
-# 1080p Download
-function fhdDownload1()
-{
-    echo -e "Downloading at 1080p - Type 1"
-    youtube-dl -f $FHDVideo1+$audio "$ytLink"
-
-}
-
-# Alternative 1080p
-# 60 FPS 
-function fhdDownload2()
-{
-    echo -e "Downloading at 1080p - Type 2"
-    youtube-dl -f $FHDVideo2+$audio "$ytLink"
-
-}
-
 
 function mainMenu() {
+
+    x=1
+
+    while [ $x=1 ]; do
 
         clear
         echo -e "${bold}${underline}Xmetal's Youtube-dl script${completeReset} \n"
 
         read -p "Youtube URL: " ytLink
 
-        echo
+        echo137
         echo -e "1. \t 720p Video Download"
         echo -e "2. \t 1080p Video Download 1"
         echo -e "3. \t 1080p Video Download 2 (60FPS Video)"
@@ -64,11 +41,32 @@ function mainMenu() {
 
         case $dlChoose in
 
-            1) hdDownload ;;
-            2) fhdDownload1 ;;
-            3) fhdDownload2 ;;
+            1)
+                video="298"
+                x=0
+                ;;
+
+            2)
+                #137---mp4  ---1920x1080  1080p 3047k , avc1.640028, 30fps, video only
+                # 1080p
+                video="137"
+                x=0
+                ;;
+
+            3)
+                #299 --- mp4  ---1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
+                # 1080p
+                # 60 FPS
+                video="299"
+                x=0
+                ;;
 
             4) return ;;
+
+            *)
+                echo -e "Invalid selection"
+                x=1
+                ;;
 
         esac
     done
