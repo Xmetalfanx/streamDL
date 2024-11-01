1cf9d9005e8a24edc83c734e22abb98987628286
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-11-01 02:36:57 -0400

    Added extract audio option for ytdownload

--- ytDownload.sh
+++ ytDownload.sh
@@ -4,17 +4,15 @@
 audio="140"
 
 # New idea for meta function
-function ytDownloadMeta()
-{
+function ytDownloadMeta() {
+    
     case video in
-
         298) echo -e "Downloading at 720p" ;;
         137) echo -e "Downloading at 1080p - Type 1" ;;
         299) echo -e "Downloading at 1080p - Type 2" ;;
-
     esac
 
-    youtube-dl -f $video+$audio "$ytLink"
+    yt-dlp -f $video+$audio "$ytLink"
 
 }
 
@@ -28,8 +26,7 @@ function mainMenu() {
         echo -e "${bold}${underline}Xmetal's Youtube-dl script${completeReset} \n"
 
         read -p "Youtube URL: " ytLink
-
-        echo137
+        
         echo -e "1. \t 720p Video Download"
         echo -e "2. \t 1080p Video Download 1"
         echo -e "3. \t 1080p Video Download 2 (60FPS Video)"
@@ -42,26 +39,28 @@ function mainMenu() {
         case $dlChoose in
 
             1)
-                video="298"
+                video="298" && ytDownloadMeta
                 x=0
                 ;;
 
             2)
-                #137---mp4  ---1920x1080  1080p 3047k , avc1.640028, 30fps, video only
+                # 137---mp4  ---1920x1080  1080p 3047k , avc1.640028, 30fps, video only
                 # 1080p
-                video="137"
+                video="137" && ytDownloadMeta
                 x=0
                 ;;
 
             3)
-                #299 --- mp4  ---1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
+                # 299 --- mp4  ---1920x1080  1080p60 3255k , avc1.64002a, 60fps, video only,
                 # 1080p
                 # 60 FPS
-                video="299"
+                video="299" && ytDownloadMeta
                 x=0
                 ;;
 
-            4) return ;;
+            4)  yt-dlp -x "$ytLink"
+                x=0
+                ;;
 
             *)
                 echo -e "Invalid selection"
